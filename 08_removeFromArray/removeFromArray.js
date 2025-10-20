43d5a1f553961da080fbbed433e61c416c8c3e20
victorbsz <victor.souza0350@gmail.com>
2025-10-20 12:57:38 -0300

    ex08 done

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    newArray = []
+    array.forEach((element) => {
+        if (!remove.includes(element)) {
+            newArray.push(element)
+        }
+        console.log(`Old array: ${array} \n New array: ${newArray}`)
+    });
+    return newArray
 };
+
+removeFromArray([1, 2, 3, 4], 2, 3)
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
