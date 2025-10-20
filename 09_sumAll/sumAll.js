5f3563c606b99e4422a5a52a6de5b3ff2a2558e4
victorbsz <victor.souza0350@gmail.com>
2025-10-20 13:25:02 -0300

    ex09 done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+    total = 0
+    if (!Number.isInteger(n1) || !Number.isInteger(n2) || n1 < 0 || n2 < 0) {
+        return "ERROR"
+    } else {  
+        if (n1 < n2) {
+            for (i = n1; i <= n2; i++) {
+                total += i
+            }
+        } else if(n1 > n2) {
+            for (i = n1; i >= n2; i--) {
+                total += i
+            }
+        }
+    }
+    console.log(`Total: ${total}`)
+    return total
 };
 
+sumAll(-2, 4)
 // Do not edit below this line
 module.exports = sumAll;
