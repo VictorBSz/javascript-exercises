24e5d2e841c00345fc1f24509c85d916f0e4777d
victorbsz <victor.souza0350@gmail.com>
2025-10-21 20:50:53 -0300

    ex 11 done

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  converted = (temp - 32) * 5 / 9
+  rounded = Math.round(converted * 10) / 10
+  return rounded
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  converted = temp * 9 / 5 + 32
+  rounded = Math.round(converted * 10) / 10
+  return rounded
 };
+
+console.log(convertToCelsius(100))
 
 // Do not edit below this line
 module.exports = {
