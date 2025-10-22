6cff6297afc8d46787579c1240d4f59b5c7adb50
victorbsz <victor.souza0350@gmail.com>
2025-10-22 13:11:15 -0300

    ex 13 done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    let gnirts = ""
+    string = input.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replaceAll(' ', '').toLowerCase();    
+    for (i = string.length -1; i >= 0; i--) {
+        gnirts += string[i]
+    }
+    console.log(`String = ${string}, inverted = ${gnirts}`)
 
+    return gnirts == string
 };
+
+console.log(palindromes("A car, a man, a maraca."))
 
 // Do not edit below this line
 module.exports = palindromes;
